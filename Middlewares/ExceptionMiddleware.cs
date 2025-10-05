c63f98b7068e4402952c969dd23ccd1c3518526b
Gabriel Svizzero <116688337+Gabrielsvi@users.noreply.github.com>
2025-10-05 18:30:11 -0300

    Add files via upload

---
+++ Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,39 @@
+using System.Net;
+using System.Text.Json;
+
+namespace LocadoraVeiculos.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Erro não tratado");
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = new { message = "Ocorreu um erro no servidor.", detail = exception.Message };
+            return context.Response.WriteAsync(JsonSerializer.Serialize(response));
+        }
+    }
+}
