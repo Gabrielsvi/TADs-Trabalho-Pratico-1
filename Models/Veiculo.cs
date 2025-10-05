ffb1d6332f5a71fa54f089039d351358091a5196
Gabriel Svizzero <116688337+Gabrielsvi@users.noreply.github.com>
2025-10-05 18:22:57 -0300

    Update Veiculo.cs

--- Models/Veiculo.cs
+++ Models/Veiculo.cs
@@ -1,13 +1,27 @@
-public class Veiculo
-{
-    public int VeiculoId { get; set; }
-    public string Modelo { get; set; } = null!;
-    public int AnoFabricacao { get; set; }
-    public decimal Quilometragem { get; set; }
-    public string Placa { get; set; } = null!;
-    public bool Disponivel { get; set; } = true;
+using System.ComponentModel.DataAnnotations;
 
-    public int FabricanteId { get; set; }
-    public Fabricante Fabricante { get; set; } = null!;
-    public ICollection<Aluguel> Alugueis { get; set; } = new List<Aluguel>();
+namespace LocadoraVeiculos.Model
+{
+    public class Veiculo
+    {
+        public int VeiculoId { get; set; }
+
+        [Required, StringLength(100)]
+        public string Modelo { get; set; } = null!;
+
+        [Range(1900, 2100)]
+        public int AnoFabricacao { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public decimal Quilometragem { get; set; }
+
+        [Required, StringLength(10)]
+        public string Placa { get; set; } = null!;
+
+        public bool Disponivel { get; set; } = true;
+
+        public int FabricanteId { get; set; }
+        public Fabricante Fabricante { get; set; } = null!;
+        public ICollection<Aluguel> Alugueis { get; set; } = new List<Aluguel>();
+    }
 }
